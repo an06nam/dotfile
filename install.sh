207b93246a88fd5dd03b01780c35d97b9c26ff62
an06nam <an.06.nam@gmail.com>
2025-07-12 17:35:24 +0700

    new-commit

---
+++ install.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+
+sudo pacman -Syu \
+	neovim \
+	chrome \
+	terminator \
+	git wget curl \
+	gcc make cmake \
+	docker docker-compose docker-buildx \
+	ca-certificates \
+	i3 rofi feh pipewire \
+	python-pip python-virtualenv \
+	-y
+
+
