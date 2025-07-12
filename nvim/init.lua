e04dbe40ab8b8abb67b5a857a83344eda46374ef
an06nam <an.06.nam@gmail.com>
2025-07-12 17:41:41 +0700

    modified:   nvim/init.lua 	new file:   rofi/config.rasi

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,12 +1,17 @@
 vim.cmd.colorscheme('unokai')
-vim.opt.number=true
-vim.opt.mouse='a'
-vim.opt.ignorecase=true
-vim.opt.smartcase=true
+vim.opt.number = true
+vim.opt.mouse = 'a'
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
 vim.opt.hlsearch = false
 vim.opt.wrap = true
 vim.opt.breakindent = true
+
+-- Tab Option
 vim.opt.tabstop = 2
 vim.opt.shiftwidth = 2
-vim.opt.expandtab = false
+vim.opt.expandtab = true
+vim.opt.autoindent = true
+
+-- Config Cursor
 vim.opt.cursorline = true
