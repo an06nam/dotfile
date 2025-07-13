010b50d42b0285499d79e17d697c5c0cbb668dea
an06nam <an.06.nam@gmail.com>
2025-07-13 09:36:29 +0700

    modified: setup script

--- install.sh
+++ install.sh
@@ -1,16 +1,75 @@
-#!/bin/bash
+#!/bin/sh
 
+#================================================
+# What Is This ?
+# A script to setup i3wm with some perks
+#================================================
 
-sudo pacman -Syu \
-	neovim \
-	chrome \
-	terminator \
-	git wget curl \
-	gcc make cmake \
-	docker docker-compose docker-buildx \
-	ca-certificates \
-	i3 rofi feh pipewire \
-	python-pip python-virtualenv \
-	-y
+get_os() {
+  if [ -f /etc/os-release ]; then
+    . /etc/os-release
+    echo "$NAME"
+  else
+    echo "Unknown"
+  fi
+}
 
+DISTRO_NAME="$(get_os)"
 
+setup_archLinux() {
+  echo "OK"
+}
+
+setup_ubuntu() {
+  echo "OK"
+}
+
+setup_debian() {
+  echo "OK"
+}
+
+setup_almalinux() {
+  echo "OK"  
+}
+
+if [ "$DISTRO_NAME" = "Arch Linux" ] || [ "$DISTRO_NAME" = "Ubuntu" ] || [ "$DISTRO_NAME" = "Debian" ] || [ "$DISTRO_NAME" = "AlmaLinux" ] || [ "$DISTRO_NAME" = "Fedora" ]; then
+  echo "Wait A moment..."
+  sleep 1
+  echo "Maybe grab some coffe..."
+  sleep 1
+else
+  echo "your distro not supported at the moment"
+fi
+  
+case "$DISTRO_NAME" in
+  "Arch Linux")
+    setup_archLinux
+    clear
+    echo "SETUP FINISHED"
+    sleep 1
+    clear
+    echo "I use Arch BTW"
+    ;;
+  "Debian")
+    setup_debian
+    ;;
+  "Ubuntu")
+    setup_ubuntu
+    ;;
+  "AlmaLinux" | "Fedora")
+    setup_almalinux
+    ;;
+esac
+
+#sudo pacman -Syu \
+#	neovim \
+#	chrome \
+#	git wget curl \
+#	gcc make cmake \
+#	docker docker-compose docker-buildx \
+#	ca-certificates \
+#  # Desktop
+#	i3 rofi feh pipewire terminator \
+#	python-pip python-virtualenv \
+#	-y
+#
